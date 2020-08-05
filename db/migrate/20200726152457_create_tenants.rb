476d89745a8b6a111077d5c7cf48cb9118ae496c
sasha370 <kng.sasha@mail.ru>
2020-08-05 11:37:35 +0300

    prepare migration heroku

--- db/migrate/20200726152457_create_tenants.rb
+++ db/migrate/20200726152457_create_tenants.rb
@@ -1,4 +1,4 @@
-class CreateTenants < ActiveRecord::Migration
+class CreateTenants < ActiveRecord::Migration[6.0]
   def change
     create_table :tenants do |t|
       t.references :tenant, index: true, foreign_key: true
