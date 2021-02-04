07a0a94ee6e123e06803c58d1a750281e3f4a5d4
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:12:43 +0300

    Update artifacts_controller.rb

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -1,29 +1,21 @@
 class ArtifactsController < ApplicationController
   before_action :set_artifact, only: [:show, :edit, :update, :destroy]
 
-  # GET /artifacts
-  # GET /artifacts.json
   def index
     @artifacts = Artifact.all
   end
 
-  # GET /artifacts/1
-  # GET /artifacts/1.json
   def show
   end
 
-  # GET /artifacts/new
   def new
     @artifact = Artifact.new
     @artifact.project_id = params[:project_id]
   end
 
-  # GET /artifacts/1/edit
   def edit
   end
 
-  # POST /artifacts
-  # POST /artifacts.json
   def create
     @artifact = Artifact.new(artifact_params)
 
@@ -36,8 +28,6 @@ class ArtifactsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /artifacts/1
-  # PATCH/PUT /artifacts/1.json
   def update
     respond_to do |format|
       if @artifact.update(artifact_params)
@@ -48,8 +38,6 @@ class ArtifactsController < ApplicationController
     end
   end
 
-  # DELETE /artifacts/1
-  # DELETE /artifacts/1.json
   def destroy
     @artifact.destroy
     respond_to do |format|
@@ -59,12 +47,10 @@ class ArtifactsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_artifact
     @artifact = Artifact.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def artifact_params
     params.require(:artifact).permit(:name, :upload, :project_id)
   end
