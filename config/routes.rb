b76341ee2499660c7291e4dfa389ec665cab3c7e
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:47:56 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,9 @@ Rails.application.routes.draw do
   resources :projects
   resources :members
 
-  # Главная страница по адресу
   root "home#index"
 
-  # Переадресация из MILIA, т.к. она автоматическпи пересылает на welcome после входа
   get 'welcome', to: 'home#index'
-
-
-
-  # Такое вложение позволяет делать URL вида tenant#/project# , т.е. одной организации может принадлежать несколько проектов
 
   resources :tenants do
     resources :projects do
@@ -22,14 +16,10 @@ Rails.application.routes.draw do
     end
   end
 
-
-  # *MUST* come *BEFORE* devise's definitions (below)
   as :user do   
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
 
-
-  # Для User испоьзуются следующие контроллеры
   devise_for :users, :controllers => { 
     :registrations => "registrations",
     :confirmations => "confirmations",
@@ -37,8 +27,7 @@ Rails.application.routes.draw do
     :passwords => "milia/passwords"
   }
 
-  # путь для изменения Тарифа
   match "/plan/edit" => 'tenants#edit', via: :get, as: :edit_plan
   match "/plan/update" => 'tenants#update', via: [:put, :patch], as: :update_plan
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
