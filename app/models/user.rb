0dd4ee5b9d7a2cc65f58515c75cf733dce68776a
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:23:23 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable,  :validatable
 
-  # Использовать кау основной Акк для Milia
   acts_as_universal_and_determines_account
-  # Имеет одного Зарегистрированного, наследует Удаление
   has_one :member, :dependent => :destroy
-  # User имеет множество взаимосвязей UserПроект
   has_many :user_projects
-  # User имеет множество проектов, через UserПроект
   has_many :projects, through: :user_projects
-  # Имеют один Платеж, делят некоторые атрибуты с Платежем
   has_one :payment
+  
   accepts_nested_attributes_for :payment
 
-
-# Проверяем админ ли пользователь? Возвращает true /false
   def self.is_admin?
     is_admin
   end
-
 end
