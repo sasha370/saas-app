0de9911e02694feff311769964a9710d91048402
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:13:44 +0300

    Update confirmations_controller.rb

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,18 +1,11 @@
 class ConfirmationsController < Milia::ConfirmationsController
-
-  # Сложный контроллер, который я до конца не понимаю, т.к. используется Milia
-
   def update
-
-    # Если пароли совпали
     if @confirmable.attempt_set_password(user_params)
 
       # this section is patterned off of devise 3.2.5 confirmations_controller#show
-      # создается запись подтверждения по token
       self.resource = resource_class.confirm_by_token(params[:confirmation_token])
       yield resource if block_given?
 
-      # Если ошибок нет, то подтверждается приглашение, высылается письмо на email
       if resource.errors.empty?
         log_action( "invitee confirmed" )
         set_flash_message(:notice, :confirmed) if is_flashing_format?
@@ -25,15 +18,12 @@ class ConfirmationsController < Milia::ConfirmationsController
         respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
       end
 
-      # Если пароли не совпали
     else
       log_action( "invitee password set failed" )
       prep_do_show()  # prep for the form
       respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :show }
     end  # if..then..else passwords are valid
   end
-
-
 
   def show
     if @confirmable.new_record?  ||
@@ -56,23 +46,17 @@ class ConfirmationsController < Milia::ConfirmationsController
       flash[:notice] = "Please choose a password and confirm it"
       prep_do_show()  # prep for the form
     end
-    # else fall thru to show template which is form to set a password
-    # upon SUBMIT, processing will continue from update
   end
 
-  # после подтверждения пароля
   def after_confirmation_path_for(resource_name, resource)
-    # Если User уже вошел
     if user_signed_in?
       root_path
     else
-      # Если не вошел, то на страницу Входа
       new_user_session_path
     end
   end
 
   private
-
 
   def set_confirmable()
     @confirmable = User.find_or_initialize_with_error_by(:confirmation_token, params[:confirmation_token])
